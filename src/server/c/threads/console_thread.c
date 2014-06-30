b65064595a889e1e1881f5dc574e802438587366
immortaleeb <immortaleeb@gmail.com>
2014-06-30 18:03:35 +0200

    Add register command to server
    Adds the register command to the server console to register new (domain,
    token) pairs.

--- src/server/c/threads/console_thread.c
+++ src/server/c/threads/console_thread.c
@@ -2,13 +2,21 @@
 
 #include "thread_args.h"
 #include "../../../common/c/console.h"
+#include "../../../common/c/common.h"
+#include "../map/dns_map.h"
 
-#define NUM_CONSOLE_COMMANDS 1
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_CONSOLE_COMMANDS 2
 #define CONSOLE_BUFFER_SIZE 255
 
 static int (*has_finished)(void);
 static void (*set_finished)(void);
+static dns_map* map;
 
+void register_func(int argc, char** argv);
 void stop_func(int argc, char** argv);
 
 void* console_thread_main(void* args) {
@@ -16,12 +24,14 @@ void* console_thread_main(void* args) {
     thread_args_t* arguments = args;
     has_finished = arguments->has_finished;
     set_finished = arguments->set_finished;
+    map = arguments->map;
 
     // Init console
     console_init(NUM_CONSOLE_COMMANDS);
 
     // Register commands
     register_command("stop", stop_func);
+    register_command("register", register_func);
 
     // Run the console
     console_run("> ", CONSOLE_BUFFER_SIZE, has_finished);
@@ -30,6 +40,36 @@ void* console_thread_main(void* args) {
     console_destroy();
 
     return NULL;
+}
+
+void register_func(int argc, char** argv) {
+    if (argc != 3) {
+        printf("Usage: %s <domain> <token>\n", argv[0]);
+        return;
+    } else {
+        char* domain;
+        char* current_token;
+        char* token;
+
+        domain = (char*) malloc(sizeof(char) * (strlen(argv[1]) + 1));
+        token = (char*) malloc(sizeof(char) * (TOKEN_SIZE + 1));
+        strcpy(domain, argv[1]);
+        memcpy(token, argv[2], TOKEN_SIZE);
+        token[TOKEN_SIZE] = '\0';
+
+        current_token = dns_map_get_token_from_domain(map, domain);
+        if (current_token != NULL) {
+            printf("domain '%s' is already linked to token '%s', registration failed\n", 
+                    domain, current_token);
+            free(domain);
+            free(token);
+            return;
+        }
+
+        dns_map_put_token(map, domain, token, 1);
+        printf("Succesfully linked domain '%s' to token '%s'\n",
+                domain, token);
+    }
 }
 
 void stop_func(int argc, char** argv) {
