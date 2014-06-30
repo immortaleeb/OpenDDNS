2f56ecb42863cec4f122698b45bb9e7fea00612d
immortaleeb <immortaleeb@gmail.com>
2014-06-30 15:51:40 +0200

    Create dns_map and add to thread arguments
    A dns_map and the corresponding files are created when the server
    starts. Also the dns_map is added to thread arguments, so that all
    threads who need access to the map have access to it.

--- src/server/c/main.c
+++ src/server/c/main.c
@@ -4,8 +4,12 @@
 #include "threads/thread_args.h"
 #include "threads/dns_thread.h"
 #include "threads/console_thread.h"
+#include "map/dns_map.h"
 
-// Indiciates wether we have executed the "stop" command
+#define DOMAIN2TOKEN_FILENAME "domain2token.data"
+#define TOKEN2IP_FILENAME "token2ip.data"
+
+// Indicates wether we have executed the "stop" command
 static int finished = 0;
 
 void set_finished();
@@ -14,29 +18,52 @@ int has_finished();
 int main(int argc, char** argv) {
     thread_args_t args;
     pthread_t dns_thread, console_thread;
+    dns_map* map;
+    FILE *h_domain2token, *h_token2ip;
     int res;
+
+    // Open necessary files
+    if ( !(h_domain2token = fopen(DOMAIN2TOKEN_FILENAME, "a+b")) ) {
+        fprintf(stderr, "Failed to open file %s: %d\n", DOMAIN2TOKEN_FILENAME, errno);
+        return 1;
+    }
+    if ( !(h_token2ip = fopen(TOKEN2IP_FILENAME, "a+b")) ) {
+        fprintf(stderr, "Failed to open file %s: %d\n", TOKEN2IP_FILENAME, errno);
+        return 2;
+    }
+
+    // Initialize dns_map
+    map = create_dns_map(h_domain2token, h_token2ip);
 
     // Create the necessary arguments for the threads
     args.has_finished = has_finished;
     args.set_finished = set_finished;
+    args.map = map;
 
     // Create dns thread
     res = pthread_create(&dns_thread, NULL, dns_thread_main, &args);
     if (res) {
         fprintf(stderr, "Error creating the DNS thread: %d\n", res);
-        return 1;
+        return 3;
     }
 
     // Create console thread
     res = pthread_create(&console_thread, NULL, console_thread_main, &args);
     if (res) {
         fprintf(stderr, "Error creating the console thread: %d\n", res);
-        return 2;
+        return 4;
     }
 
     // Wait for all threads to finish
     pthread_join(dns_thread, NULL);
     pthread_join(console_thread, NULL);
+
+    // Destroy the dns map
+    destroy_dns_map(map);
+
+    // Close open files
+    fclose(h_domain2token);
+    fclose(h_token2ip);
 
     return 0;
 }
--- src/server/c/threads/thread_args.h
+++ src/server/c/threads/thread_args.h
@@ -1,9 +1,12 @@
 #ifndef THREAD_ARGS_H_
 #define THREAD_ARGS_H_
 
+#include "../map/dns_map.h"
+
 typedef struct thread_args {
     int (*has_finished)(void);
     void (*set_finished)(void);
+    dns_map* map;
 } thread_args_t;
 
 #endif
